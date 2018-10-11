d04fe5982f22dd90c16d0cd8cf60a0c8e7b50e2f
Bruno dos Santos Carvalhal <bruno.dos.santos.carvalhal@ordina.nl>
2018-10-11 10:35:45 +0200

    Changed project to typescript

--- src/ing.ts
+++ src/ing.ts
@@ -1,38 +1,45 @@
 const https = require('https');
 const axios = require('axios');
 const crypto = require('crypto');
-const fs = require('fs');
 const qs = require('qs');
 
 import { guid } from "./utils";
 
-// Constants
-let SANDBOX, apiBasePath, signingKey, signingPassphrase, tlsCert, tlsKey, clientId, agent;
-
 export default class ING {
 
-    constructor(useSandbox, _signingKey, _signingPassphrase, _tlsCertificate, _tlsKey, _clientId) {
-        SANDBOX = useSandbox;
-        apiBasePath = useSandbox ? "https://api.sandbox.ing.com" : "https://api.ing.com";
-        signingKey = _signingKey;
-        signingPassphrase = _signingPassphrase;
-        tlsCert = _tlsCertificate;
-        tlsKey = _tlsKey;
-        clientId = _clientId;
+    // Member variables
+    SANDBOX: boolean;
+    apiBasePath: string;
+    signingKey: string;
+    signingPassphrase: string;
+    tlsCert: string;
+    tlsKey: string;
+    clientId: string;
+    agent: any;
+
+
+    constructor(useSandbox: boolean, _signingKey: string, _signingPassphrase: string, _tlsCertificate: string, _tlsKey: string, _clientId: string) {
+        this.SANDBOX = useSandbox;
+        this.apiBasePath = useSandbox ? "https://api.sandbox.ing.com" : "https://api.ing.com";
+        this.signingKey = _signingKey;
+        this.signingPassphrase = _signingPassphrase;
+        this.tlsCert = _tlsCertificate;
+        this.tlsKey = _tlsKey;
+        this.clientId = _clientId;
 
         // Create HTTPS agent
-        agent = new https.Agent({
+        this.agent = new https.Agent({
             rejectUnauthorized: false,
-            cert: tlsCert,
-            key: tlsKey
+            cert: this.tlsCert,
+            key: this.tlsKey
         });
     }
 
-    async requestShowcase(access_token) {
+    async requestShowcase(access_token: string) {
         return await this.requestAPI("get", "/greetings/single", access_token);
     }
 
-    async requestAuthorizationUrl(scope, countryCode, accessToken) {
+    async requestAuthorizationUrl(scope: string, countryCode: string, accessToken: string) {
         const urlParams = qs.stringify({
             scope,
             "country_code": countryCode
@@ -41,8 +48,8 @@ export default class ING {
         return result.location;
     }
 
-    async requestCustomerAccessToken(authorizationCode, accessToken) {
-        if (SANDBOX) {
+    async requestCustomerAccessToken(authorizationCode: string, accessToken: string) {
+        if (this.SANDBOX) {
             authorizationCode = "694d6ca9-1310-4d83-8dbb-e819c1ee6b80";
         }
 
@@ -56,22 +63,22 @@ export default class ING {
         return result.access_token;
     }
 
-    async requestAccounts(customerAccessToken) {
+    async requestAccounts(customerAccessToken: string) {
         const result = await this.requestAPI("get", "/v1/accounts", customerAccessToken);
         return result.accounts;
     }
 
-    async requestBalances(customerAccessToken, accountId) {
+    async requestBalances(customerAccessToken: string, accountId: string) {
         const result = await this.requestAPI("get", "/v1/accounts/" + accountId + "/balances", customerAccessToken);
         return result.accounts;
     }
 
-    async requestTransactions(customerAccessToken, accountId) {
+    async requestTransactions(customerAccessToken: string, accountId: string) {
         const result = await this.requestAPI("get", "/v1/accounts/" + accountId + "/transactions", customerAccessToken);
         return result.accounts;
     }
 
-    async requestAPI(httpMethod, reqPath, access_token, bodyData = {}) {
+    async requestAPI(httpMethod: string, reqPath: string, access_token: string, bodyData = {}) {
 
         // URL encode body
         const body = qs.stringify(bodyData);
@@ -89,15 +96,15 @@ export default class ING {
             method: httpMethod,
             headers: {
                 'Authorization': `Bearer ${access_token}`,
-                'Signature': `keyId="${clientId}",algorithm="rsa-sha256",headers="(request-target) date digest x-ing-reqid",signature="${signature}"`,
+                'Signature': `keyId="${this.clientId}",algorithm="rsa-sha256",headers="(request-target) date digest x-ing-reqid",signature="${signature}"`,
                 'X-ING-ReqID': reqId,
                 'Date': dateString,
                 'Content-Type': 'application/x-www-form-urlencoded',
                 'Digest': `SHA-256=${digest}`
             },
             data: body,
-            url: apiBasePath + reqPath,
-            httpsAgent: agent
+            url: this.apiBasePath + reqPath,
+            httpsAgent: this.agent
         };
 
         // Return result
@@ -106,7 +113,7 @@ export default class ING {
     }
 
     // Request Access Token
-    async requestAccessToken(scope) {
+    async requestAccessToken(scope: string) {
         const bodyData = {
             grant_type: 'client_credentials',
             scope
@@ -114,7 +121,7 @@ export default class ING {
 
         // URL encode body
         let body = qs.stringify(bodyData);
-        if (SANDBOX) {
+        if (this.SANDBOX) {
             body = 'grant_type=client_credentials&scope=create_order+granting+payment-requests+payment-requests%3Aview+payment-requests%3Acreate+payment-requests%3Aclose+virtual-ledger-accounts%3Afund-reservation%3Acreate+virtual-ledger-accounts%3Afund-reservation%3Adelete+virtual-ledger-accounts%3Abalance%3Aview';
         }
 
@@ -130,31 +137,31 @@ export default class ING {
         let options = {
             method: 'post',
             headers: {
-                'Authorization': `Signature keyId="${clientId}",algorithm="rsa-sha256",headers="(request-target) date digest x-ing-reqid",signature="${signature}"`,
+                'Authorization': `Signature keyId="${this.clientId}",algorithm="rsa-sha256",headers="(request-target) date digest x-ing-reqid",signature="${signature}"`,
                 'X-ING-ReqID': reqId,
                 'Date': dateString,
                 'Content-Type': 'application/x-www-form-urlencoded',
                 'Digest': `SHA-256=${digest}`
             },
             data: body,
-            url: apiBasePath + "/oauth2/token",
-            httpsAgent: agent
+            url: this.apiBasePath + "/oauth2/token",
+            httpsAgent: this.agent
         };
 
         // SANDBOX API is a bit different :?
-        if (SANDBOX) {
+        if (this.SANDBOX) {
             options = {
                 method: 'post',
                 headers: {
-                    'Signature': `keyId="${clientId}",algorithm="rsa-sha256",headers="(request-target) date digest x-ing-reqid",signature="${signature}"`,
+                    'Signature': `keyId="${this.clientId}",algorithm="rsa-sha256",headers="(request-target) date digest x-ing-reqid",signature="${signature}"`,
                     'X-ING-ReqID': reqId,
                     'Date': dateString,
                     'Content-Type': 'application/x-www-form-urlencoded',
                     'Digest': `SHA-256=${digest}`
-                },
+                } as any,
                 data: body,
-                url: apiBasePath + "/oauth2/token",
-                httpsAgent: agent
+                url: this.apiBasePath + "/oauth2/token",
+                httpsAgent: this.agent
             };
         }
 
@@ -163,11 +170,11 @@ export default class ING {
         return result.data.access_token;
     }
 
-    generateSignature(httpMethod, reqPath, dateString, digest, reqId) {
+    generateSignature(httpMethod: string, reqPath: string, dateString: string, digest: string, reqId: string) {
         const signingString = `(request-target): ${httpMethod} ${reqPath}\ndate: ${dateString}\ndigest: SHA-256=${digest}\nx-ing-reqid: ${reqId}`;
 
         const hash = crypto.createSign('SHA256');
         hash.update(signingString);
-        return hash.sign({ key: signingKey, passphrase: signingPassphrase }, "base64");
+        return hash.sign({ key: this.signingKey, passphrase: this.signingPassphrase }, "base64");
     }
 }
--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,5 @@
-export function guid() {
-    function s4() {
+export function guid(): string {
+    function s4(): string {
         return Math.floor((1 + Math.random()) * 0x10000)
             .toString(16)
             .substring(1);
